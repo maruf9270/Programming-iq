806405095547704621450803834d5ebfb227b66b
Maruf9270 <Maruf9270@gmail.com>
2022-10-11 20:04:38 +0600

    Added about page content

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div>
-            About us section
+        <div className='w-full h-[100vh] flex justify-center items-center flex-col'>
+            <div className='text-3xl'>
+                We are the best of the best in the world
+            </div>
+            <div className='w-[90%] my-14'>
+                <p className='text-center'>
+                    We are the largest teach teaching company among the wrold. We have the world class mentor. Our mentor will show you the path for your success.
+                     Our mentor try their best to teach every student. This is why we are the best. Our studens are working at big teach compay around the world.
+                </p>
+            </div>
+            
         </div>
     );
 };
